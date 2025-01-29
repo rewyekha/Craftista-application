8a3d0a27728985300ff747419e976b88d3eeee4a
charliepoker <iheanachocharlie@gmail.com>
2025-01-29 12:07:28 -0500

    modified  eslint.config.js

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,13 +1,16 @@
-module.exports = [
+import eslintConfigRecommended from "eslint/configurations/recommended.js";
+import eslintPluginReact from "eslint-plugin-react/configurations/recommended.js";
+import eslintPluginReactHooks from "eslint-plugin-react-hooks/configurations/recommended.js";
+import eslintPluginPrettier from "eslint-plugin-prettier/configurations/recommended.js";
+import eslintPluginTypescript from "@typescript-eslint/eslint-plugin/configurations/recommended.js";
+
+export default [
   {
     files: ["*.js", "*.jsx", "*.ts", "*.tsx"],
-    extends: [
-      "eslint:recommended",
-      "plugin:@typescript-eslint/recommended",
-      "plugin:react/recommended",
-      "plugin:react-hooks/recommended",
-      "plugin:prettier/recommended",
-    ],
+    parserOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+    },
     rules: {
       "no-console": "warn",
       "no-unused-vars": "error",
@@ -15,5 +18,11 @@ module.exports = [
       "react/jsx-uses-react": "error",
       "react/jsx-uses-vars": "error",
     },
+    plugins: ["@typescript-eslint", "react", "react-hooks", "prettier"],
+    ...eslintConfigRecommended,
+    ...eslintPluginReact,
+    ...eslintPluginReactHooks,
+    ...eslintPluginPrettier,
+    ...eslintPluginTypescript,
   },
 ];
