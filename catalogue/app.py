d72d47ab3808b0cec3ea080f9b12c9142fc7e577
Tony Csernok <toonynatoor@gmail.com>
2025-02-25 17:44:26 +0100

    feat(catalogue): migrate product data source from PostgreSQL to MongoDB

--- catalogue/app.py
+++ catalogue/app.py
@@ -3,50 +3,32 @@ from datetime import datetime
 import socket
 import os
 import json
-import psycopg2
+from pymongo import MongoClient
 
 app = Flask(__name__)
 
-# Load product data from JSON file
-with open('products.json', 'r') as f:
-    products = json.load(f)
 with open("config.json", "r") as config_file:
-    config_data = json.load(config_file) 
+    config_data = json.load(config_file)
 
-def get_db_connection():
-    conn = psycopg2.connect(host=config_data.get("db_host"),
-                            database=config_data.get("db_name"),
-                            user=config_data.get("db_user"),
-                            password=config_data.get("db_password")
-                           )
-    return conn
+mongo_uri = f"mongodb://{config_data['db_user']}:{config_data['db_password']}@{config_data['db_host']}:27017/{config_data['db_name']}?authSource=admin"
+mongo_client = MongoClient(mongo_uri)
+db = mongo_client[config_data["db_name"]]
 
 @app.route('/')
 def home():
     system_info = get_system_info()
-    app_version = config_data.get("app_version", "N/A")  # Default to "N/A" if no version is found
+    app_version = config_data.get("app_version", "N/A")
     return render_template('index.html', current_year=datetime.now().year, system_info=system_info, version=app_version)
 
 @app.route('/api/products', methods=['GET'])
-
 def get_products():
-    if (config_data.get("data_source") == "db"): 
-        conn = get_db_connection()
-        cur = conn.cursor()
-        cur.execute('SELECT * FROM products;')
-        db_products = cur.fetchall()
-        products_dict = [{'id': curr_product[0], 'description': curr_product[1], 'image_url': curr_product[2], 'name': curr_product[3]} for curr_product in db_products]
-        cur.close()
-        conn.close()
-        return jsonify(products_dict), 200
-    else:
-        return jsonify(products), 200
-
+    product_list = list(db.products.find({}, {'_id': 0}))
+    return jsonify(product_list), 200
 
 @app.route('/api/products/<int:product_id>', methods=['GET'])
 def get_product(product_id):
-    product = next((product for product in products if product['id'] == product_id), None)
-    if product is not None:
+    product = db.products.find_one({"id": product_id}, {'_id': 0})
+    if product:
         return jsonify(product)
     else:
         return jsonify({'message': 'Product not found'}), 404
@@ -54,11 +36,8 @@ def get_product(product_id):
 def get_system_info():
     hostname = socket.gethostname()
     ip_address = socket.gethostbyname(hostname)
-
-    # Additional logic for container and Kubernetes check
     is_container = os.path.exists('/.dockerenv')
     is_kubernetes = os.path.exists('/var/run/secrets/kubernetes.io/serviceaccount')
-    
     return {
         "hostname": hostname,
         "ip_address": ip_address,
@@ -66,7 +45,5 @@ def get_system_info():
         "is_kubernetes": is_kubernetes
     }
 
-
 if __name__ == "__main__":
     app.run(debug=True)
-
